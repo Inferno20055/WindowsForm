e066e87ec71070018272a3f9eeb71c421199c873
Inferno20055 <baanovaleksej676@gmail.com>
2025-05-23 16:54:01 +0900

    Add files via upload

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace DatePickerExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            datePicker.IsDropDownOpen = false;
+        }
+
+        
+        private void datePicker_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (datePicker.SelectedDate.HasValue)
+            {
+                labelSelectedDate.Content = "Выбранная дата: " + datePicker.SelectedDate.Value.ToString("dd.MM.yyyy");
+            }
+            else
+            {
+                labelSelectedDate.Content = "Дата не выбрана";
+            }
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            datePicker.IsDropDownOpen = true; 
+        }
+    }
+}
